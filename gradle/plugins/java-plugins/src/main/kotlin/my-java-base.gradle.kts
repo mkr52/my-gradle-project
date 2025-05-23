aa638b10678f9d8306eafe69319830178e062180
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-23 13:47:22 +0530

    configure tasks added by plugins

--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -1,3 +1,5 @@
+import org.apache.tools.ant.taskdefs.Java
+
 plugins {
     id("java")
     id("com.diffplug.spotless")
@@ -8,4 +10,25 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.compileJava {
+    options.release.set(21)
+
+}
+
+// Another way to set the task via a named feature. Equivalent to "tasks.compileTestJava{}"
+tasks.named<JavaCompile>("compileTestJava") {
+}
+
+tasks.test {
+
+}
+
+tasks.javadoc {
+
 }
