50f1665fdc317c35e9f30148d5c3d3f63afc7193
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-05-23 14:03:00 +0530

    register new tasks using existing task implementations

--- gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
@@ -2,3 +2,13 @@ plugins {
     id("application")
     id("my-java-base")
 }
+
+tasks.register<Zip>("bundle") {
+    group = "My Group"
+    description = "package it all!"
+
+    from(tasks.jar) // includes the main classes
+    from(configurations.runtimeClasspath) // includes runtime dependencies
+
+    destinationDirectory.set(layout.buildDirectory.dir("distribution"))
+}
